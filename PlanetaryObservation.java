0edf3e5cf8922d033217af96c887cf73fe6d95a9
coltbrand <czjazzkirb@gmail.com>
2017-04-04 10:45:55 -0500

    Add files via upload

--- PlanetaryObservation.java
+++ PlanetaryObservation.java
@@ -9,36 +9,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class PlanetaryObservation extends Application {
   @Override 
   public void start(Stage primaryStage) {
-    GridPane pane = new GridPane();
-    pane.setAlignment(Pos.CENTER);
-    pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
-    pane.setHgap(5.5);
-    pane.setVgap(5.5);
+    GridPane basePane = new GridPane();
+    basePane.setAlignment(Pos.CENTER);
+    basePane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+    basePane.setHgap(5.5);
+    basePane.setVgap(5.5);
     
     
-    /*
-    // Place nodes in the pane
-    pane.add(new Label("First Name:"), 0, 0); // column, row
-    pane.add(new TextField(), 1, 0);
-    pane.add(new Label("MI:"), 0, 1); 
-    pane.add(new TextField(), 1, 1);
-    pane.add(new Label("Last Name:"), 0, 2);
-    pane.add(new TextField(), 1, 2);
-    Button btAdd = new Button("Add Name");
-    pane.add(btAdd, 2, 3);
-    GridPane.setHalignment(btAdd, HPos.LEFT);
-    */
     
     // Create a scene and place it in the stage
-    Scene scene = new Scene(pane);
+    Scene mainScene = new Scene(basePane,500,450);
+    primaryStage.setResizable(false);
+    primaryStage.setAlwaysOnTop(true);
     primaryStage.setTitle("Planetary Observation"); // Set the stage title
-    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.setScene(mainScene); // Place the scene in the stage
     primaryStage.show(); // Display the stage
   }
   
